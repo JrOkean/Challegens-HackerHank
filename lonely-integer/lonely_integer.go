65c4f74158ea0cea898d79f96fb9921688e972d4
jrokean <amarelaxx@gmail.com>
2024-07-05 03:39:04 -0300

    revisando a linguagem

---
+++ lonely-integer/lonely_integer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func lonelyinteger(a []int32) int32 {
+	var b int32
+	b = a[0]
+	//len_vector = len(a)
+
+	return b
+}
+
+func main() {
+	s := `Hello
+					playground
+							  hasld`
+	fmt.Printf("%v\n%T\n", s, s)
+	b := "Hello, playground"
+	bb := []byte(b)
+	for _, v := range bb {
+		fmt.Printf("%v - %T - %#U - %#x", v, v, v, v)
+	}
+}
