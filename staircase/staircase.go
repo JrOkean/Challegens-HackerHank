d89502328024df2c910422cc34e888e88e2a4d0b
jrokean <amarelaxx@gmail.com>
2024-09-09 13:22:07 -0300

    DOCS: structure

---
+++ staircase/staircase.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func staircase(n int32) {
+	// Write your code here
+
+}
+
+func main() {
+	fmt.Print("Hello, world")
+	staircase(n)
+}
